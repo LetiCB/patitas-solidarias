34514fd4c2fbfacd7db3c1f5e4ecf06a99d8c7b1
LetiCB <lcastrobrisson@gmail.com>
2023-05-20 20:00:31 -0300

    Agregar conexión a The Cat API

--- funciones.js
+++ funciones.js
@@ -1,4 +1,18 @@
-
+// --- APIs --- //
+// Obtener un arreglo con 10 urls de fotos de gatos
+async function obtenerFotosDeGatos() {
+    try {
+      let response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
+      let data = await response.json();
+      let gatosUrls = data.map(imageData => imageData.url);
+      return gatosUrls;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+let fotosDeGatos = obtenerFotosDeGatos();
+console.log(fotosDeGatos);
 
 
 

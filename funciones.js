db1245b505df9555d6d79db2033d872951ee8b0a
LetiCB <lcastrobrisson@gmail.com>
2023-05-19 21:05:00 -0300

    Agregar funcionalidad en JavaScript para mostrar año actual en footer

---
+++ funciones.js
@@ -0,0 +1,11 @@
+
+
+
+
+// --- Footer --- //
+
+// Obtener el año actual
+const anio = new Date().getFullYear();
+
+// Mostrar el año actual en el footer.
+document.getElementById('anio-actual').textContent = anio;

682de56c5043c45b3a14afd18e7a7d9b6fe6f5b8
LetiCB <lcastrobrisson@gmail.com>
2023-05-20 21:39:00 -0300

    Agregar función para mostrar gatos en adopción

--- funciones.js
+++ funciones.js
@@ -1,18 +1,59 @@
 // --- APIs --- //
-// Obtener un arreglo con 10 urls de fotos de gatos
-async function obtenerFotosDeGatos() {
-    try {
-      let response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
-      let data = await response.json();
-      let gatosUrls = data.map(imageData => imageData.url);
-      return gatosUrls;
-    } catch (error) {
-      console.log(error);
-    }
-  }
+// Conectar a la API de gatos y solicitar 10 urls de imágenes
+fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+      .then(response => response.json())
+      .then(data => data.map(imageData => imageData.url))
+      .then(data => {
+        // Obtener un arreglo con 10 urls de fotos de gatos
+        let fotosDeGatos = data;
+        let nombreGato = ["Luna", "Milo", "Simba", "Nala", "Tigre", "Mia", "Leo", "Coco", "Bellota", "Max"];
+        let telGato = ["+54 444 45678", "+54 234 56789", "+54 345 67890", "+54 456 78901", "+54 567 89012", "+54 677 90123", "+54 789 01234", "+54 890 12345", "+54 901 23456", "+54 012 34567"];
+ 
+        // Crear un arreglo de objetos con el teléfono de contacto, nombre y foto de cada gato en adopción.
+        let gatosEnAdopcion = [];
+        for (let i = 0; i < nombreGato.length; i++) {
+            let gatoEnAdopcion = {
+                nombre: nombreGato[i],
+                telefono: telGato[i],
+                foto: fotosDeGatos[i],
+            };
+            gatosEnAdopcion.push(gatoEnAdopcion);
+        }
+
+        console.log(gatosEnAdopcion);
+          
+        // Ubicar el contenedor donde se mostrarán los gatos en adopción en el DOM
+        let gatosEnAdopcionDOM = document.getElementById("gatos-en-adopcion");
+          
+        // Función para renderizar tarjetas con datos de animales 
+        function renderizarTarjetaDeAnimales(dataDeAnimales, contenedor) {
+            contenedor.innerHTML = dataDeAnimales
+            .map((animal) => {
+                return `
+                <div class="tarjeta">
+                    <div class="foto"><img src="${animal.foto}" alt="foto de ${animal.nombre}"/></div>
+                    <div class="nombre">${animal.nombre}</div>
+                    <div class="telefono">${animal.telefono}</div>
+                </div>`;
+            })
+            .join(" ");
+        }
+        
+        renderizarTarjetaDeAnimales(gatosEnAdopcion, gatosEnAdopcionDOM);
+
+
+
+
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
   
-let fotosDeGatos = obtenerFotosDeGatos();
-console.log(fotosDeGatos);
+
+
+
+// let perrosEnAdopcionDOM = document.getElementById("perros-en-adopcion");
 
 
 

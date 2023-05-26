7fa619d214381bcbb2eb23648349e66ab6e1476c
LetiCB <lcastrobrisson@gmail.com>
2023-05-26 17:22:58 -0300

    Agregar boton para volver arriba en index.html con estilo y script

--- funciones.js
+++ funciones.js
@@ -1,3 +1,21 @@
+// --- Boton para volver arriba --- //
+
+window.addEventListener('scroll', function() {
+    const botonVolverArriba = document.querySelector('.volver-arriba');
+    if (window.scrollY > 300) {
+        botonVolverArriba.classList.add('show');
+    } else {
+        botonVolverArriba.classList.remove('show');
+    }
+  });
+  
+  document.querySelector('.volver-arriba').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
+
+
 // --- Footer --- //
 
 // Obtener el año actual
